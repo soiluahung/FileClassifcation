3735749b0f8296ee5be8fb2b636f79a4fd120414
Thong Nguyen Thanh <thongnguyen050999@gmail.com>
2019-06-19 18:50:45 +0700

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ CURR_CONFIG = {}
 with open(PDF_TYPE + '/' + PDF_TYPE + '.json', 'r', encoding='utf8') as json_file:
     CONFIG = json.load(json_file)
 
+# KEYWORD for VN103933
 KEYWORD = [
             'SHIPPING REQUEST', 'Shipper', 'Consignee', 'Notify', 'Reference No.', 'Booking No.', 'TO  ',
             'TEL  ','FAX  ','ATTN', 'FROM', 'E-MAIL', 'M.B/L Type', 'HS Code', 'M.Vessel/Voyage',
@@ -19,6 +20,7 @@ KEYWORD = [
             'No of Package', 'Description of Goods', 'Weight', 'Measurement', 'FREIGHT PREPAID'
             ]
 
+# KEYWORD FOR VN101466
 # KEYWORD = ['From','To','Booking No.','Date','Shipper Name & Address',
 #             'Consignee Name & Address','Notify Party','Port of Loading',
 #             'Port of Discharge','Place of Delivery','Vessel',
